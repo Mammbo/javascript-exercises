9f2cc3b8297222c33503856992bb7d37f7a583bf
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-06-29 14:42:46 -0500

    calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => total * currentItem);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1
+  } 
+  let product = 1 
+  for (i = num; i > 0; i--){
+    product *= i
+  }
+  return product
 };
 
 // Do not edit below this line
